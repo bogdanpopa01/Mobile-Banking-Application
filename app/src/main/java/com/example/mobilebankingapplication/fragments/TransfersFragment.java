1baf23f7e6d426a7ef168d31bc989e75c412e4b0
Bogdan Popa <90871325+bogdanpopa01@users.noreply.github.com>
2023-03-15 20:45:37 +0200

    added the design for the card

--- app/src/main/java/com/example/mobilebankingapplication/fragments/TransfersFragment.java
+++ app/src/main/java/com/example/mobilebankingapplication/fragments/TransfersFragment.java
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.mobilebankingapplication.R;
 
 public class TransfersFragment extends Fragment {
+    EditText etAmountTranfersFragment, etPayeeTransfersFragment, etIBANTransfersFragment, etDescriptionTransfersActivity;
+
     public TransfersFragment() {
         // Required empty public constructor
     }
