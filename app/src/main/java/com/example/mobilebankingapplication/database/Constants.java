908fba316240c108a4d9b30b54453012c8f4834c
Bogdan Popa <90871325+bogdanpopa01@users.noreply.github.com>
2023-03-30 18:52:26 +0300

    Transfer - post method working

---
+++ app/src/main/java/com/example/mobilebankingapplication/database/Constants.java
@@ -0,0 +1,6 @@
+package com.example.mobilebankingapplication.database;
+
+public class Constants {
+    private static final String ROOT_URL = "http://192.168.31.71/MobileBankingApplication/v1/";
+    public static final String URL_REGISTER_TRANSFER = ROOT_URL + "registerTransfer.php";
+}
--- app/src/main/java/com/example/mobilebankingapplication/fragments/TransfersFragment.java
+++ app/src/main/java/com/example/mobilebankingapplication/fragments/TransfersFragment.java
@@ -2,6 +2,7 @@ package com.example.mobilebankingapplication.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 
@@ -16,11 +17,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mobilebankingapplication.R;
 import com.example.mobilebankingapplication.classes.Transfer;
+import com.example.mobilebankingapplication.database.Constants;
 import com.example.mobilebankingapplication.utils.RandomLongGenerator;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class TransfersFragment extends Fragment {
@@ -52,11 +67,45 @@ public class TransfersFragment extends Fragment {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validation()){
+                if (validation()) {
                     Transfer transfer = createTransfer();
-                    if(transfer!=null){
-                        Bundle bundle = new Bundle();
-                        bundle.putParcelable(TransactionsFragment.KEY_SEND_TRANSACTION,transfer);
+                    if (transfer != null) {
+                        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                                Constants.URL_REGISTER_TRANSFER,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String response) {
+                                        try {
+                                            JSONObject jsonObject = new JSONObject(response);
+                                            Toast.makeText(getContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                                        } catch (JSONException e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }) {
+                            @Nullable
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                Map<String, String> params = new HashMap<>();
+                                params.put("transferId", String.valueOf(transfer.getTransferId()));
+                                params.put("transferAmount", String.valueOf(transfer.getTransferAmount()));
+                                params.put("transferIsInvoice", String.valueOf(transfer.isTransferIsInvoice()));
+                                params.put("transferPayee", transfer.getTransferPayee());
+                                params.put("transferIBAN", transfer.getTransferIBAN());
+                                params.put("transferDescription", transfer.getTransferDescription());
+                                params.put("userId", String.valueOf(transfer.getUserId()));
+                                return params;
+                            }
+                        };
+
+                        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+                        requestQueue.add(stringRequest);
+
                     }
 
                 }
@@ -67,30 +116,30 @@ public class TransfersFragment extends Fragment {
         return view;
     }
 
-    private void initializeComponents(){
+    private void initializeComponents() {
         etAmountTranfersFragment = view.findViewById(R.id.etAmountTransfersFragment);
         etIBANTransfersFragment = view.findViewById(R.id.etIBANTransfersFragment);
         etPayeeTransfersFragment = view.findViewById(R.id.etPayeeTransfersFragment);
         etDescriptionTransfersActivity = view.findViewById(R.id.etDescriptionTransfersFragment);
         switchIsInvoice = view.findViewById(R.id.switchIsInvoice);
         spinnerCompanies = view.findViewById(R.id.spinnerCompanies);
-        ArrayAdapter<CharSequence> arrayAdapterSpinner = ArrayAdapter.createFromResource(getContext(),R.array.array_companies, android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> arrayAdapterSpinner = ArrayAdapter.createFromResource(getContext(), R.array.array_companies, android.R.layout.simple_spinner_dropdown_item);
         spinnerCompanies.setAdapter(arrayAdapterSpinner);
         btnSend = view.findViewById(R.id.btnSendTransfersFragment);
     }
 
-    private Transfer createTransfer(){
+    private Transfer createTransfer() {
         long transferId = RandomLongGenerator.generateLong();
         double transferAmount = Double.parseDouble(etAmountTranfersFragment.getText().toString());
         boolean isInvoice = switchIsInvoice.isChecked();
         String transferPayee = etPayeeTransfersFragment.getText().toString();
-        String transferIBAN = etIBANTransfersFragment.getText().toString().replace(" ","");
+        String transferIBAN = etIBANTransfersFragment.getText().toString().replace(" ", "");
         String transferDescription = etDescriptionTransfersActivity.getText().toString();
         long userId = RandomLongGenerator.generateLong();
 
-        Transfer transfer = new Transfer(transferId,transferAmount,isInvoice,transferPayee,transferIBAN,transferDescription,userId);
+        Transfer transfer = new Transfer(transferId, transferAmount, isInvoice, transferPayee, transferIBAN, transferDescription, userId);
 
-        if(transfer!=null){
+        if (transfer != null) {
             return transfer;
         }
 
@@ -101,20 +150,20 @@ public class TransfersFragment extends Fragment {
         }
     }
 
-    private boolean validation(){
+    private boolean validation() {
         boolean isValid = true;
 
-        if(etAmountTranfersFragment.getText() == null || etAmountTranfersFragment.getText().toString().trim().isEmpty()){
+        if (etAmountTranfersFragment.getText() == null || etAmountTranfersFragment.getText().toString().trim().isEmpty()) {
             etAmountTranfersFragment.setError("The amount field cannot be empty!");
             isValid = false;
         }
 
-        if(etPayeeTransfersFragment.getText() == null || etPayeeTransfersFragment.getText().toString().trim().isEmpty()){
+        if (etPayeeTransfersFragment.getText() == null || etPayeeTransfersFragment.getText().toString().trim().isEmpty()) {
             etPayeeTransfersFragment.setError("The payee field cannot be empty!");
             isValid = false;
         }
 
-        if(etIBANTransfersFragment.getText() == null || etIBANTransfersFragment.getText().toString().trim().isEmpty()){
+        if (etIBANTransfersFragment.getText() == null || etIBANTransfersFragment.getText().toString().trim().isEmpty()) {
             etIBANTransfersFragment.setError("The IBAN field cannot be empty!");
             isValid = false;
         }
@@ -122,7 +171,7 @@ public class TransfersFragment extends Fragment {
         return isValid;
     }
 
-    private void transferAmountValidation(){
+    private void transferAmountValidation() {
 
         etAmountTranfersFragment.addTextChangedListener(new TextWatcher() {
             @Override
@@ -131,7 +180,7 @@ public class TransfersFragment extends Fragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() > 0 && !isValidDepositAmount(s.toString())){
+                if (s.length() > 0 && !isValidDepositAmount(s.toString())) {
                     String string = validString(s.toString());
                     etAmountTranfersFragment.setText(string);
                     etAmountTranfersFragment.setSelection(string.length());
@@ -148,13 +197,14 @@ public class TransfersFragment extends Fragment {
         return string.matches("[0-9.]*");
     }
 
-    private String validString(String string){
+    private String validString(String string) {
         return string.replaceAll("[^0-9.]", "");
     }
 
-    private void transferIBANValidation(){
+    private void transferIBANValidation() {
         etIBANTransfersFragment.addTextChangedListener(new TextWatcher() {
             final char space = ' ';
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
@@ -167,7 +217,7 @@ public class TransfersFragment extends Fragment {
             public void afterTextChanged(Editable s) {
                 String ibanRegex = "RO\\d{2}[A-Z]{4}\\d{16}";
                 String string = s.toString();
-                if (!(string.replace(" ","")).matches(ibanRegex)) {
+                if (!(string.replace(" ", "")).matches(ibanRegex)) {
                     etIBANTransfersFragment.setError("The format of the IBAN is invalid!");
                 }
 
