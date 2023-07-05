e7ac02ad684103ef4a7e0f29def7eb90097877e5
Bogdan Popa <90871325+bogdanpopa01@users.noreply.github.com>
2023-07-05 15:53:05 +0300

    weekly_sum function

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,4 +1,36 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def prediction(amounts, dates):
-    return type(datetime.strptime(dates[0], '%Y-%m-%d').date())
+    expenses_dates = []
+    expenses_amounts = []
+    
+    # Convert dates to datetime objects
+    for i in range(len(dates)):
+        expenses_dates.append(datetime.strptime(dates[i], '%Y-%m-%d').date())
+        expenses_amounts.append(amounts[i])
+
+    start_date = expenses_dates[0]
+    end_date = expenses_dates[len(expenses_dates)-1]
+
+    # Calculate the number of days between start_date and end_date
+    num_days = (end_date - start_date).days
+
+    # Iterate over each whole week
+    weekly_sums = []
+    for i in range(num_days // 7):
+        week_start = start_date + timedelta(days=i * 7)
+        week_end = week_start + timedelta(days=6)
+
+        # Find expenses within the week range
+        weekly_expenses = [
+            expense_amount
+            for expense_date, expense_amount in zip(expenses_dates, expenses_amounts)
+            if week_start <= expense_date <= week_end
+        ]
+
+        # Calculate the sum of weekly expenses
+        weekly_sum = sum(weekly_expenses)
+        weekly_sums.append(weekly_sum)
+
+
+
