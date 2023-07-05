217d5cf1d4df8514151780c8be1d774cb06dccc5
Bogdan Popa <90871325+bogdanpopa01@users.noreply.github.com>
2023-07-05 16:39:25 +0300

    monthtly linear regression

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+from sklearn.linear_model import LinearRegression
+import random
+import numpy as np
 
 def prediction(amounts, dates):
     expenses_dates = []
@@ -15,22 +18,44 @@ def prediction(amounts, dates):
     # Calculate the number of days between start_date and end_date
     num_days = (end_date - start_date).days
 
-    # Iterate over each whole week
-    weekly_sums = []
-    for i in range(num_days // 7):
-        week_start = start_date + timedelta(days=i * 7)
-        week_end = week_start + timedelta(days=6)
+    # Iterate over each whole month
+    monthly_sums = []
+    monthly_dates = []
+    for i in range(num_days // 30):  # Assuming each month has 30 days
+        month_start = start_date + timedelta(days=i * 30)
+        month_end = month_start + timedelta(days=29)
 
-        # Find expenses within the week range
-        weekly_expenses = [
+        monthly_dates.append(month_start)
+
+        # Find expenses within the month range
+        monthly_expenses = [
             expense_amount
             for expense_date, expense_amount in zip(expenses_dates, expenses_amounts)
-            if week_start <= expense_date <= week_end
+            if month_start <= expense_date <= month_end
         ]
 
-        # Calculate the sum of weekly expenses
-        weekly_sum = sum(weekly_expenses)
-        weekly_sums.append(weekly_sum)
+        # Calculate the sum of monthly expenses
+        monthly_sum = sum(monthly_expenses)
+        monthly_sums.append(monthly_sum)
 
+    # Prepare the data for linear regression
+    X = [[i] for i in range(len(monthly_sums))]
+    y = monthly_sums
 
+    # Create a linear regression model and fit the data
+    regression_model = LinearRegression()
+    regression_model.fit(X, y)
 
+    # Predict the future monthly sum
+    future_month = len(monthly_sums)  # Assuming prediction for the next month
+    future_month_sum = regression_model.predict([[future_month]])
+
+    # Add white noise to the predicted value
+    noise_std = 0.1 * np.mean(monthly_sums)
+    noise = random.gauss(0, noise_std)
+    future_month_sum += noise
+
+    # Round the predicted value to two decimal places
+    future_month_sum = round(future_month_sum[0], 2)
+
+    return future_month_sum
